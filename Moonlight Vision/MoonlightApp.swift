dafa4ec628b54c5747914042ad73bbd1e5935345
Alex Haugland <alex.haugland@gmail.com>
2024-01-22 13:39:31 -0500

    fix openssl common import and start adapting appdelegate

--- Moonlight Vision/MoonlightApp.swift
+++ Moonlight Vision/MoonlightApp.swift
@@ -4,6 +4,8 @@ import SwiftUI
 
 @main
 struct MoonlightApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             MainContentView()
